406f5ce760bb812ac8697952f14b104f24698a9e
yusufmumani <yusufmumani@gmail.com>
2021-10-20 22:36:40 +0300

    Update AhoCorasickCaseInsensitive.java
    Redone

--- AhoCorasickCaseInsensitive.java
+++ AhoCorasickCaseInsensitive.java
@@ -1,9 +1,11 @@
+package com.venim;
+
 import java.util.*;
 
 class Node {
     Node parent;
     String key;
-    char ch;
+    Character ch;
     List<Node> childLinks = new ArrayList<>();
     Map<Character, Node> childLinksMap = new HashMap<>();
     Node suffixLink;
@@ -15,50 +17,49 @@ class Node {
         childLinksMap.put(Character.toLowerCase(node.ch), node);
     }
 
-    Node(char ch) {
+    Node(Character ch) {
         this.ch = ch;
-    }
-
-    public void setIsRoot(boolean isRoot) {
-        this.isRoot = isRoot;
+        this.isRoot = ch == null;
     }
 }
 
 public class AhoCorasickCaseInsensitive {
-    static String input = "Fuck you money";
-    static Node root;
-    static String[] keys = {"MONeY"};
+    Node root;
 
     public static void main(String[] args) {
-        root = new Node('\0');
-        root.setIsRoot(true);
+        AhoCorasickCaseInsensitive ahoCorasickCaseInsensitive = new AhoCorasickCaseInsensitive();
+        ahoCorasickCaseInsensitive.query("MO Ne y NeY");
+        ahoCorasickCaseInsensitive.search("Fuck you money").stream()
+                .flatMap(List::stream)
+                .map(String::valueOf)
+                .forEach(System.out::println);
+    }
+
+    public void query(String query) {
+        root = new Node(null);
+        String[] keys = query.split("\\s+");
         for (String key : keys) {
             createChildLinks(root, key, 0);
         }
         createSuffixAndDictLinks(root);
-        search(input);
     }
 
-    public static void createChildLinks(Node node, String key, int pos) {
+    public void createChildLinks(Node node, String key, int pos) {
         if (key.length() == pos)
             return;
-        for (Node child : node.childLinks) {
-            if (child.ch == key.charAt(pos)) {
-                createChildLinks(child, key, pos + 1);
-                return;
-            }
+        Node child = node.childLinksMap.get(Character.toLowerCase(key.charAt(pos)));
+        if (child == null) {
+            child = new Node(key.charAt(pos));
+            node.addChildLink(child);
         }
-
-        Node child = new Node(key.charAt(pos));
-        child.inDict = pos == key.length() - 1;
-        if (child.inDict) {
+        if (!child.inDict) {
+            child.inDict = pos == key.length() - 1;
             child.key = key;
         }
-        node.addChildLink(child);
         createChildLinks(child, key, pos + 1);
     }
 
-    public static void createSuffixAndDictLinks(Node root) {
+    public void createSuffixAndDictLinks(Node root) {
         Queue<Node> Q = new LinkedList<>();
         Q.offer(root);
         while (!Q.isEmpty()) {
@@ -89,12 +90,11 @@ public class AhoCorasickCaseInsensitive {
         }
     }
 
-    public static boolean isMyParentOrRoot(Node suffixLink, Node me) {
-        for (Node child : suffixLink.childLinks) {
-            if (child.ch == me.ch) {
-                me.suffixLink = child;
-                return true;
-            }
+    public boolean isMyParentOrRoot(Node suffixLink, Node me) {
+        Node child = suffixLink.childLinksMap.get(Character.toLowerCase(me.ch));
+        if (child != null) {
+            me.suffixLink = child;
+            return true;
         }
         if (suffixLink.isRoot) {
             me.suffixLink = suffixLink;
@@ -103,19 +103,41 @@ public class AhoCorasickCaseInsensitive {
         return false;
     }
 
-    public static void search(String string) {
+    public List<List<List>> search(String string) {
+        List<List<List>> results = new ArrayList<>();
         Node curr = root;
         for (int pos = 0; pos < string.length(); pos++) {
             char chAtPos = string.charAt(pos);
-
             if (curr.childLinksMap.containsKey(Character.toLowerCase(chAtPos))) {
                 curr = curr.childLinksMap.get(Character.toLowerCase(chAtPos));
                 if (curr.inDict) {
-                    System.out.println(curr.key + " at end pos " + pos);
+                    List<Object> list1 = new ArrayList<>();
+                    list1.add(pos - curr.key.length() + 1);
+                    list1.add(pos);
+                    
+                    List<Object> list2 = new ArrayList<>();
+                    list2.add(string.substring(pos - curr.key.length() + 1, pos + 1));
+
+                    List<List> list3 = new ArrayList<>();
+                    list3.add(list1);
+                    list3.add(list2);
+                    results.add(list3);
                 }
+
                 Node dictionaryLink = curr.dictionaryLink;
                 while (dictionaryLink != null) {
-                    System.out.println(dictionaryLink.key + " at end pos " + pos);
+                    List<Object> list1 = new ArrayList<>();
+                    list1.add(pos - dictionaryLink.key.length() + 1);
+                    list1.add(pos);
+
+                    List<Object> list2 = new ArrayList<>();
+                    list2.add(string.substring(pos - dictionaryLink.key.length() + 1, pos + 1));
+
+                    List<List> list3 = new ArrayList<>();
+                    list3.add(list1);
+                    list3.add(list2);
+                    results.add(list3);
+
                     dictionaryLink = dictionaryLink.dictionaryLink;
                 }
             } else {
@@ -125,5 +147,28 @@ public class AhoCorasickCaseInsensitive {
                 }
             }
         }
+        // Absorb overlaps
+        Map<Integer, Boolean> toBeRemoved = new HashMap<>();
+        List<Integer> toBeRemovedArr = new ArrayList<>();
+        for (int i = 0; i < results.size(); i++)
+            for (int j = 0; j < results.size(); j++)
+                if (i != j) {
+                    Integer startI = (Integer) results.get(i).get(0).get(0);
+                    Integer endI = (Integer) results.get(i).get(0).get(1);
+                    Integer startJ = (Integer) results.get(j).get(0).get(0);
+                    Integer endJ = (Integer) results.get(j).get(0).get(1);
+                    if (startJ >= startI && endJ <= endI)
+                        if (toBeRemoved.get(j) == null || !toBeRemoved.get(j)) {
+                            toBeRemoved.put(j, true);
+                            toBeRemovedArr.add(j);
+                        }
+                }
+
+        if (toBeRemovedArr.size() > 0) {
+            Collections.sort(toBeRemovedArr, Collections.reverseOrder());
+            for (Integer i : toBeRemovedArr)
+                results.remove(i.intValue());
+        }
+        return results;
     }
 }
